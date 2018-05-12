59bdaf100e2640eb6395142e23bbdd017bb8352b
Estefania <tefajarita@gmail.com>
2018-05-12 16:48:07 -0500

    Changes mongoose validator

--- models/usuario.js
+++ models/usuario.js
@@ -3,11 +3,11 @@ var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
 var usuarioSchema = new Schema({
-    nombre: { type: String, require: [true, 'El nombre es necesario'] },
-    email: { type: String, unique: true, require: [true, 'El nombre es necesario'] },
-    password: { type: String, require: [true, 'La contraseña es necesario'] },
-    img: { type: String, require: false },
-    role: { type: String, require: true, default: 'USER_ROLE' }
+    nombre: { type: String, required: [true, 'El nombre es necesario'] },
+    email: { type: String, unique: true, required: [true, 'El nombre es necesario'] },
+    password: { type: String, required: [true, 'La contraseña es necesario'] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE' }
 });
 //Ingreso login se coloca path para el mensaje se dinamico
 usuarioSchema.plugin(uniqueValidator, { message: 'El correo debe de ser único' })
