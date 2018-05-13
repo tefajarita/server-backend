39a33b6c4a335cfd19b75f68d4e860eca967f77d
Estefania <tefajarita@gmail.com>
2018-05-13 14:16:55 -0500

    Configuracion ambientes

--- config/config.js
+++ config/config.js
@@ -22,10 +22,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Base de datos
 //=====================================
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/bolsaempleo';
-//} else {
-urlDB = 'mongodb://bolsa-user:123456@ds147070.mlab.com:47070/bolsaempleo';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/bolsaempleo';
+} else {
+    urlDB = 'mongodb://bolsa-user:123456@ds147070.mlab.com:47070/bolsaempleo';
+}
 
 process.env.URLDB = urlDB;
